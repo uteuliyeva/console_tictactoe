80f183e37505c661207d06400fd742bf63c67e92
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2022-03-15 14:07:43 +0600

    Another version of tictactoe, but with the use of pointers instead of references. Created as a practice exercise from Chapter 7 of Beginning C++ Through Game Programming by Michael Dawson.

---
+++ ver_pointers.cpp
@@ -0,0 +1,205 @@
+//Console Tic-Tac-Toe2.0
+//Computer plays against user
+//Practice exercise from Beginning C++ Through Game Programming by Michael Dawson
+//This version is similar to the main, but pointers are used instead of references
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//global char variables
+const char X='X';
+const char O='O';
+const char EMPTY=' ';
+const char TIE='T';
+const char NO_ONE='N';
+
+//function prototypes
+void showRules();
+void showBoard(const vector<char>* const pBoard);
+int askMove(string question, int high,int low=1);
+char getHumanPiece();
+char getOpponentPiece(char piece);
+char getWinner(const vector<char>* const pBoard);
+bool isLegalMove(const vector<char>* const pBoard, int move);
+int getHumanMove(const vector<char>* const pBoard, char humanPiece);
+int getComputerMove(vector<char> board, char computerPiece);
+void announceWinner(char winner, char computerPiece, char humanPiece);
+
+int main(){
+  int move;
+  const int N_SQUARES=9;
+  vector<char> board(N_SQUARES,EMPTY);
+
+  showRules();
+  char humanPiece=getHumanPiece();
+  char computerPiece=getOpponentPiece(humanPiece);
+  char turnNow=X;
+
+  showBoard(&board);
+  while(getWinner(&board)==NO_ONE){
+    if (turnNow==humanPiece){
+      move=getHumanMove(&board,humanPiece);
+      board[move]=humanPiece;
+    }
+    else{
+      move=getComputerMove(board,computerPiece);
+      board[move]=computerPiece;
+    }
+    showBoard(&board);
+    turnNow=getOpponentPiece(turnNow);
+  }
+  announceWinner(getWinner(&board),computerPiece,humanPiece);
+  return 0;
+}
+
+void showRules(){
+  cout<<"---TIC-TAC-TOE---\n\n";
+  cout<<"Here you can make moves by choosing a number 1-9.\n";
+  cout<<"Each number corresponds to the position on the board\n";
+  cout<<"\t 1 | 2 | 3 "<<endl;
+  cout<<"\t ----------"<<endl;
+  cout<<"\t 4 | 5 | 6 "<<endl;
+  cout<<"\t ----------"<<endl;
+  cout<<"\t 7 | 8 | 9 "<<endl;
+}
+
+char getHumanPiece(){
+  char response;
+  string question="Please choose your piece. Press X or O. X goes first.";
+  do{
+    cout<<"\n"<<question<<"\n";
+    cin>>response;
+  }while(response!='x'&&response!='X'&&response!='o'&&response!='O');
+  return toupper(response);
+}
+
+
+char getOpponentPiece(char piece){
+  if (piece==X){
+    return O;
+  }
+  else{
+    return X;
+  }
+}
+
+void showBoard(const vector<char>* const pBoard){
+  cout<<"\n\t "<<(*pBoard)[0]<<" | "<<(*pBoard)[1]<<" | "<<(*pBoard)[2];
+  cout<<"\n\t ----------";
+  cout<<"\n\t "<<(*pBoard)[3]<<" | "<<(*pBoard)[4]<<" | "<<(*pBoard)[5];
+  cout<<"\n\t ----------";
+  cout<<"\n\t "<<(*pBoard)[6]<<" | "<<(*pBoard)[7]<<" | "<<(*pBoard)[8];
+  cout<<"\n\n";
+}
+
+char getWinner(const vector<char>* const pBoard){
+  //all possible winning rows
+  const int WINNING_ROWS[8][3]={{0,1,2},{3,4,5},{6,7,8},
+  {0,3,6},{1,4,7},{2,5,8},
+  {0,4,8},{2,4,6}};
+  const int N_ROWS=8;
+  //check whether one of the rows is filled with same pieces
+  for (int row=0;row<N_ROWS;++row){
+    if((*pBoard)[WINNING_ROWS[row][0]]!=EMPTY&&
+      (*pBoard)[WINNING_ROWS[row][0]]==(*pBoard)[WINNING_ROWS[row][1]]&&
+      (*pBoard)[WINNING_ROWS[row][1]]==(*pBoard)[WINNING_ROWS[row][2]]){
+        return (*pBoard)[WINNING_ROWS[row][0]];
+      }
+  }
+  //check for tie
+  if (count((*pBoard).begin(),(*pBoard).end(),EMPTY)==0){
+    return TIE;
+  }
+  //no one is winner yet
+  return NO_ONE;
+}
+
+inline bool isLegalMove(const vector<char>* const pBoard, int move){
+  return ((*pBoard)[move]==EMPTY);
+}
+
+int askMove(string question, int high,int low){
+  int move;
+  do{
+    cout<<question<<" ("<<low<<"-"<<high<<"):";
+    cin>>move;
+  }while(move>high||move<low);
+  return move-1;
+}
+
+int getHumanMove(const vector<char>* const pBoard, char humanPiece){
+  string question="Enter your next move.";
+  int move=askMove(question,(*pBoard).size());
+  while(!isLegalMove(pBoard,move)){
+    cout<<"That position is occupied.\n";
+    move=askMove("Please choose again.",(*pBoard).size());
+  }
+  cout<<"Ok!";
+  return move;
+}
+
+int getComputerMove(vector<char> board, char computerPiece){
+  unsigned int move=0;
+  bool foundMove=false;
+
+  //if computer can win on this move, make the move
+  while(!foundMove && move<board.size()){
+    if (isLegalMove(&board,move)){
+      board[move]=computerPiece;
+      foundMove=getWinner(&board)==computerPiece;
+      board[move]=EMPTY;
+    }
+    if (!foundMove){
+      move++;
+    }
+  }
+
+  //else if human can win on next move, then block the move
+  if (!foundMove){
+    move=0;
+    char humanPiece=getOpponentPiece(computerPiece);
+    while(!foundMove&&move<board.size()){
+      if(isLegalMove(&board,move)){
+        board[move]=humanPiece;
+        foundMove=getWinner(&board)==humanPiece;
+        board[move]=EMPTY;
+      }
+      if(!foundMove){
+        move++;
+      }
+    }
+  }
+  //else move is the best possible option out of free squares
+  //first check the availability of the central square, then corners and then the rest of squares
+  if(!foundMove){
+    move=0;
+    unsigned int i=0;
+    const int MOVES_BY_PRIORITY[]={4,0,2,6,8,1,3,5,7};
+
+    while(!foundMove && i<board.size()){
+      move=MOVES_BY_PRIORITY[i];
+      if(isLegalMove(&board,move)){
+        foundMove=true;
+      }
+      i++;
+    }
+  }
+  cout<<"Computer takes the square number "<<move+1<<endl;
+  return move;
+}
+
+void announceWinner(char winner, char computerPiece, char humanPiece){
+  if (winner==computerPiece){
+    cout<<"Unfortunately, you lost."<<endl;
+  }
+  else if (winner==humanPiece){
+    cout<<"Congratulations! You won!"<<endl;
+  }
+  else{
+    cout<<"It is a tie!"<<endl;
+  }
+}
